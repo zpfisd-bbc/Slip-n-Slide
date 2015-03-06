1ebfea0905d5fd426728f73cb6a8a1f884eefbbf
Tim Killenberger <Tim.Killenberger@bbcag.ch>
2015-03-06 10:23:47 +0100

    0.0.0.1.2

--- src/LineGenerator.java
+++ src/LineGenerator.java
@@ -1,18 +1,25 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
-public class LineGenerator extends Main {	
+public class LineGenerator extends Main{	
 	
-	// ID
-	private static final long serialVersionUID = -8999417439148765241L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3059159587358238294L;
 
 	// Generates Random Lines
-	public void generateLine(Graphics g) {
+	@Override
+	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		
 		g2d.setColor(Color.BLUE);
-		g2d.drawLine(10, 20, 10, 20);
-	}
+		g2d.fillOval(0, 0, 30, 30);
+		g2d.drawOval(0, 50, 30, 30);		
+		g2d.fillRect(50, 0, 30, 30);
+		g2d.drawRect(50, 50, 30, 30);
 
+		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main extends JPanel {
+public class Main extends JPanel  {
 
 	// Serial ID
 	private static final long serialVersionUID = 4648172894076113183L;
-
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Slip 'n' Slide");
+		
 		frame.add(new Main());
 		frame.setSize(1920, 1050);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}	
+	
+	public void paint(Graphics g) {
+		LineGenerator l = new LineGenerator();
+		l.paint(g);
 	}
-	
-	
-	LineGenerator line = new LineGenerator();	
-	
 }
