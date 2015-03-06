1d01d2437f63fc15f36397aab2d32a79310c1db7
Tim Killenberger <Tim.Killenberger@bbcag.ch>
2015-03-06 08:25:05 +0100

    Main Window

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JPanel {
+
+	// Serial ID
+	private static final long serialVersionUID = 4648172894076113183L;
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Slip 'n' Slide");
+		frame.add(new Main());
+		frame.setSize(1920, 1050);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
