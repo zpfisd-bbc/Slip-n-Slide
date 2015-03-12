dc7740fb26860c541db52a56703e636bdffea779
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-03-12 15:29:03 +0100

    V0.1.3.1

--- src/LineGenerator.java
+++ src/LineGenerator.java
@@ -3,34 +3,31 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.Random;
 
-public class LineGenerator extends Main {
+public class LineGenerator {
 
-	private static final long serialVersionUID = -6509408290457265056L;
-
-	int y1 = 230; //Länge von Abstand zwischen Linien
-	int l1 = 0; //Länge von linie 1
-	int l2 = 0; //Länge von line 2
-	int l3 = 100; //Länge von Abstand
-	int lastZahl = 0; //Speichert die letzte Zahl von ZufallsZahl
+	
+	int y1 = 230; // Länge von Abstand zwischen Linien
+	int l1 = 1; // Länge von linie 1
+	int l2 = 1; // Länge von line 2
+	int l3 = 100; // Länge von Abstand
+	int lastZahl = 0; // Speichert die letzte Zahl von ZufallsZahl
+	int rz; // Die Zufallszahl
 
 	// Generates Random Lines
-	@Override
 	public void paint(Graphics g) {
 		Graphics2D g1d = (Graphics2D) g;
 		Graphics2D border = (Graphics2D) g;
 		Graphics2D g3d = (Graphics2D) g;
 
-		for (int i = 1; i < 10; i++) { //Testschlaufe, mit playerDead != true
-			
-			int rz; //Die Zufallszahl
-			
+		for (int i = 1; i < 10; i++) { // Testschlaufe, mit playerDead != true
+
 			Random randomZahl = new Random();
 			rz = randomZahl.nextInt(6 - 1) + 1; // wählt eine Zufallszahl zwischen 1 - 5
 
-			while (rz == lastZahl) { //Überprüft dass keine Lücke zweimal vorkommt
+			while (rz == lastZahl) { // Überprüft dass keine Lücke zweimal vorkommt
 				rz = randomZahl.nextInt(6 - 1) + 1; // wählt eine Zufallszahl zwischen 1 - 5
 			}
-			
+
 			if (rz == 1) {
 				l1 = 0;
 				l2 = 400;
@@ -47,34 +44,35 @@ public class LineGenerator extends Main {
 				l1 = 400;
 				l2 = 0;
 			}
-			
-			//Line 1
+
+			// Line 1
 			g1d.setColor(Color.LIGHT_GRAY);
 			g1d.fillRect(10, y1, l1, 15);
 			g1d.drawRect(10, y1, l1, 15);
 
-			g1d.setColor(Color.GRAY); //Schatteneffekt für Line 1
+			g1d.setColor(Color.GRAY); // Schatteneffekt für Line 1
 			g1d.fillRect(10, y1 + 8, l1, (int) 7.5);
 			g1d.drawRect(10, y1 + 8, l1, (int) 7.5);
 
-			//Line 2
+			// Line 2
 			g3d.setColor(Color.LIGHT_GRAY);
 			g3d.fillRect(l3 + l1, y1, l2, 15);
 			g3d.drawRect(l3 + l1, y1, l2, 15);
-			
-			g3d.setColor(Color.GRAY); //Schatteneffekt für Line 2
+
+			g3d.setColor(Color.GRAY); // Schatteneffekt für Line 2
 			g3d.fillRect(l3 + l1, y1 + 8, l2, (int) 7.5);
 			g3d.drawRect(l3 + l1, y1 + 8, l2, (int) 7.5);
-			
-			y1 += 250; //Erhöht Grösse von Abstand zwischen der kompletten Linien
-			lastZahl = rz; //Speichert die letzte Zufallszahl
-			
-			//Border links
+
+			y1 += 250; // Erhöht Grösse von Abstand zwischen der kompletten
+						// Linien
+			lastZahl = rz; // Speichert die letzte Zufallszahl
+
+			// Border links
 			border.setColor(Color.LIGHT_GRAY);
 			border.fillRect(0, 0, 10, 1080);
 			border.drawRect(0, 0, 10, 1080);
-			
-			//Border rechts
+
+			// Border rechts
 			border.setColor(Color.LIGHT_GRAY);
 			border.fillRect(500, 0, 10, 1080);
 			border.drawRect(500, 0, 10, 1080);
--- src/Player.java
+++ src/Player.java
@@ -1,33 +1,109 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
-
-/**
- * 
- */
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 
 /**
  * @author Dominic Pfister, ICT Berufsbildungscenter AG, dominic.pfister@bbcag.ch
  *
  */
-class Player extends Main {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	public void drawPlayer(Graphics g) { //Zeichnet ein dreieck
-		Graphics2D g2d = (Graphics2D) g;
-		
-		g2d.setPaint(Color.cyan);
-		g2d.draw(new Line2D.Double(50, 150, 150, 150));
-		g2d.draw(new Line2D.Double(50, 150, 150, 150));
-		g2d.draw(new Line2D.Double(50, 150, 150, 150));
+public class Player {
+	private int x;
+	private int y;
+	private Image image;
+	private String pfad;
+	private int dx = 0;
+	private int dy = 0;
+
+
+	public Player() {
+		this.setPfad("Triangle.png"); //Setzt den Pfad für den Sprite
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(pfad));
+		this.setImage(ii.getImage()); //Wandelt ImageIcon in Image um
 	}
 	
-	public static void move(String direction) {
+	public void move() {
+        x += dx;
+        y += dy;
+    }
+	
+	public void keyPressed(KeyEvent e) {
+
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_LEFT) {
+			dx = -3;
+		}
+
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 3;
+		}
+
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_LEFT) {
+			dx = 0;
+		}
+
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 0;
+		}
 	}
 	
+	public void resetState(){
+		this.setX(518 / 2);
+		this.setY(500);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public String getPfad() {
+		return pfad;
+	}
+
+	public void setPfad(String pfad) {
+		this.pfad = pfad;
+	}
+
+	public int getDx() {
+		return dx;
+	}
+
+	public void setDx(int dx) {
+		this.dx = dx;
+	}
+
+	public int getDy() {
+		return dy;
+	}
+
+	public void setDy(int dy) {
+		this.dy = dy;
+	}
+
 }
