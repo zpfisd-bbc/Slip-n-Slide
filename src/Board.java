a2ac2377e2d0edff7dd40adaa69eaa7802616284
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-03-20 13:30:41 +0100

    V0.3
    - Redundant lines
    - New images
    - Player.java not working
    - Lines now generate seperately

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,11 @@
+import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
  * @author Dominic Pfister, ICT Berufsbildungscenter AG,
@@ -10,55 +13,64 @@ import javax.swing.JFrame;
  * @version 1.0
  */
 
-public class Board extends JFrame {
+public class Board extends JFrame implements ActionListener {
 
 	// SerialID
 	private static final long serialVersionUID = 1L;
 
 	// Variablen
-	// private boolean inGame = true;
-	private List<Line> lines = new ArrayList<Line>();
+	private Line l = new Line();
+	private Line l2 = new Line();
+	private Line l3 = new Line();
+	private Line l4 = new Line();
+	private Line l5 = new Line();
 
 	public Board() {
 
 		// Initialisiert das JFrame
-		// getContentPane().setBackground(Color.YELLOW); // Setzt die
-		// Hintergrundfarbe
+		getContentPane().setBackground(Color.YELLOW); //Setzt die Hintergrundfarbe
 		setSize(517, 1000); // Breite und Länge von Fenster
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
+		
+		Timer timer = new Timer(25, this);
+		timer.start();
 	}
+	
+    public void paint(Graphics g) {
+    	
 
-	public void paint(Graphics g) {
-		Line l1 = new Line(g, 230);
-		Line l2 = new Line(g, 430);
-		Line l3 = new Line(g, 630);
-		Line l4 = new Line(g, 830);
-		lines.add(l1);
-		lines.add(l2);
-		lines.add(l3);
-		lines.add(l4);
-
-		while (true) {
-			play(g);
-		}
-	}
+        super.paint(g);
+        Graphics2D g1 = (Graphics2D)g;
+        
+        
+        g1.drawImage(l.getImageL(), Line.getX(), l.getY(), this);
+        g1.drawImage(l.getImageR(), l.getImageL().getWidth(null) +  Line.getLinienAbstand() , l.getY(), this);
+        
+        g1.drawImage(l2.getImageL(), Line.getX(), l2.getY() + 200, this);
+        g1.drawImage(l2.getImageR(), l2.getImageL().getWidth(null) +  Line.getLinienAbstand() , l2.getY() + 200, this);
+        
+        g1.drawImage(l3.getImageL(), Line.getX(), l3.getY() + 400, this);
+        g1.drawImage(l3.getImageR(), l3.getImageL().getWidth(null) +  Line.getLinienAbstand() , l3.getY() + 400, this);
+        
+        g1.drawImage(l4.getImageL(), Line.getX(), l4.getY() + 600, this);
+        g1.drawImage(l4.getImageR(), l4.getImageL().getWidth(null) +  Line.getLinienAbstand() , l4.getY() + 600, this);
+        
+        g1.drawImage(l5.getImageL(), Line.getX(), l5.getY() + 800, this);
+        g1.drawImage(l5.getImageR(), l5.getImageL().getWidth(null) +  Line.getLinienAbstand() , l5.getY() + 800, this);
+    }
 
 	public static void main(String[] args) {
-		Board b = new Board(); // Fügt alle Methoden aus Board() hinzu
+		new Board(); // Fügt alle Methoden aus Board() hinzu
 	}
 
-	public void play(Graphics g) {
-		
-		try {
-			Thread.sleep(1000); // Setzt die ms in der die Funtkion aufgerufen
-								// wird
-		} catch (InterruptedException e) {
-		}
-
-		for (Line l : lines) {
-			l.scrollUp(g);
-		}
-	}
+    public void actionPerformed(ActionEvent e) { //Funktion wird alle 5ms aufgerufen
+        l.move();
+        l2.move();
+        l3.move();
+        l4.move();
+        l5.move();
+        repaint();
+    }
 }
--- src/Line.java
+++ src/Line.java
@@ -1,158 +1,180 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Image;
 import java.util.Random;
+import javax.swing.ImageIcon;
 
 /**
- * @author Dominic Pfister, ICT Berufsbildungscenter AG,
- *         dominic.pfister@bbcag.ch
+ * @author Dominic Pfister, ICT Berufsbildungscenter AG, dominic.pfister@bbcag.ch
  * @version 1.0
  */
 
 public class Line {
-	private final static int ABSTAND_LAENGE = 100; // Verändert sich nicht
-	private int zufallsZahl = 0;
-	private static int lastZahl = 0;
-
-	private int längeLn1 = 0;
-	private int längeLn2 = 0;
-	private int linienAbstand = 30; //abstand zwischen ganzen Linien
-
-	public Line(Graphics g2, int abstand) {
-		zufallsZahl = this.randomZahl(6);
-		linienAbstand = abstand;
-
-		while (zufallsZahl == lastZahl) { // Überprüft dass keine Lücke zweimal vorkommt
-			zufallsZahl = this.randomZahl(6);
-		}
-
-		// Wählt die Länge der Linien aus aufgrund von der zufallsZahl
+	
+	//Variablen
+	private int zufallsZahl;
+	private String pfad1; //Pfad für Linie 1
+	private String pfad2; //Pfad für Linie 2
+	private Image imageL;
+	private Image imageR;
+	private static int linienAbstand = 100;
+	private static int x = 20;
+	private int y = 100;
+	private int yMove;
+	private int scrollSpeed = 1; //Geschwindigkeit für Bewegen von Linien
+	private int lastZahl; //Speichert die letzte Zufallszahl
+	
+	public Line() {
+		this.randomZahl(5);
+		this.pathSelector();
+        ImageIcon lnL = new ImageIcon(this.getClass().getResource(pfad1));
+        ImageIcon lnR = new ImageIcon(this.getClass().getResource(pfad2));
+		this.setImageL(lnL.getImage());
+		this.setImageR(lnR.getImage());
+		this.setLastZahl(zufallsZahl);
+	}
+	
+    /**
+     * Moves the line
+    */
+	public void move() {
+		y += - scrollSpeed;  
+	}
+	
+    /**
+     * Selects a random number between 1 and "maxZahl"
+     *
+     * @param  maxZahl highest possible number
+     */
+	public void randomZahl(int maxZahl) { 
+		Random rz = new Random();
+		zufallsZahl = rz.nextInt(maxZahl) + 1;
+	}
+	
+    /**
+     * Selects the path for images
+     */
+	public void pathSelector() {
 		switch (zufallsZahl) {
 		case 1:
-			this.setLängeLn1(0);
-			this.setLängeLn2(400);
+			this.setPfad1("/images/0px.png");
+			this.setPfad2("/images/400px.jpg");
 			break;
 		case 2:
-			this.setLängeLn1(100);
-			this.setLängeLn2(300);
+			this.setPfad1("/images/100px.jpg");
+			this.setPfad2("/images/300px.jpg");
 			break;
 		case 3:
-			this.setLängeLn1(200);
-			this.setLängeLn2(300);
+			this.setPfad1("/images/200px.jpg");
+			this.setPfad2("/images/200px.jpg");
 			break;
 		case 4:
-			this.setLängeLn1(300);
-			this.setLängeLn2(100);
+			this.setPfad1("/images/300px.jpg");
+			this.setPfad2("/images/100px.jpg");
 			break;
 		case 5:
-			this.setLängeLn1(400);
-			this.setLängeLn2(0);
+			this.setPfad1("/images/400px.jpg");
+			this.setPfad2("/images/0px.png");
 			break;
 		}
-		paintLine(g2);
-		lastZahl = zufallsZahl; // Speichert die letzte Zufallszahl
-		linienAbstand += 200;
+		System.out.println(zufallsZahl);
 	}
 
-	public void paintLine(Graphics g2) {
-		// Linie 1 Left
-		g2.setColor(Color.GRAY);
-		g2.drawRect(10, linienAbstand, längeLn1, 15);
-		g2.fillRect(10, linienAbstand, längeLn1, 15);
-
-		g2.setColor(Color.LIGHT_GRAY); // Schatteneffekt
-		g2.drawRect(10, linienAbstand, längeLn1, (int) 7.5);
-		g2.fillRect(10, linienAbstand, längeLn1, (int) 7.5);
-
-		// Line 1 Right
-		g2.setColor(Color.GRAY);
-		g2.drawRect(ABSTAND_LAENGE + längeLn1, linienAbstand, längeLn2, 15);
-		g2.fillRect(ABSTAND_LAENGE + längeLn1, linienAbstand, längeLn2, 15);
-
-		g2.setColor(Color.LIGHT_GRAY); // Schatteneffekt
-		g2.fillRect(ABSTAND_LAENGE + längeLn1, linienAbstand, längeLn2,
-				(int) 7.5);
-		g2.fillRect(ABSTAND_LAENGE + längeLn1, linienAbstand, längeLn2,
-				(int) 7.5);
+	
+	//Getter und setter
+	public String getPfad1() {
+		return pfad1;
 	}
 
-	// wählt eine Zufallszahl zwischen 1 - 5
-	public int randomZahl(int maxZahl) {
-		Random rz = new Random();
-		return rz.nextInt(maxZahl);
+	public void setPfad1(String pfad1) {
+		this.pfad1 = pfad1;
 	}
 
-	public void scrollUp(Graphics g2) {
-		int linienAbstand = this.getLinienAbstand();
-		this.setLinienAbstand(linienAbstand - 1 - 600);
-
-		g2.clearRect(0, 0, 517, 1000); // Nur aktuelle Linie
-
-		if (linienAbstand >= 1000) {
-			linienAbstand = 150;
-			this.randomZahl(5);
-
-			while (zufallsZahl == lastZahl) { // Überprüft dass keine Lücke
-												// zweimal vorkommt
-				this.randomZahl(5);
-			}
-
-			// Setzt die Linienlängen
-			switch (zufallsZahl) {
-			case 1:
-				this.setLängeLn1(0);
-				this.setLängeLn2(400);
-				break;
-			case 2:
-				this.setLängeLn1(100);
-				this.setLängeLn2(300);
-				break;
-			case 3:
-				this.setLängeLn1(200);
-				this.setLängeLn2(300);
-				break;
-			case 4:
-				this.setLängeLn1(300);
-				this.setLängeLn2(100);
-				break;
-			case 5:
-				this.setLängeLn1(400);
-				this.setLängeLn2(0);
-				break;
-			}
-		}
-
-		paintLine(g2);
-
-		lastZahl = zufallsZahl; // Speichert die letzte Zufallszahl
-		linienAbstand += 150;
+	public String getPfad2() {
+		return pfad2;
 	}
 
-	public void paint(Graphics g) {
-
+	public void setPfad2(String pfad2) {
+		this.pfad2 = pfad2;
 	}
 
-	private int getLängeLn1() {
-		return längeLn1;
+	public int getZufallsZahl() {
+		return zufallsZahl;
 	}
 
-	private void setLängeLn1(int längeLn1) {
-		this.längeLn1 = längeLn1;
+	public void setZufallsZahl(int zufallsZahl) {
+		this.zufallsZahl = zufallsZahl;
+	}
+	
+	public int getY() {
+		return y;
 	}
 
-	private int getLängeLn2() {
-		return längeLn2;
+	
+	public void setY(int y) {
+		this.y = y;
 	}
 
-	private void setLängeLn2(int längeLn2) {
-		this.längeLn2 = längeLn2;
+	
+	public int getyMove() {
+		return yMove;
 	}
 
-	private int getLinienAbstand() {
+	
+	public void setyMove(int yMove) {
+		this.yMove = yMove;
+	}
+
+	
+	public int getScrollSpeed() {
+		return scrollSpeed;
+	}
+
+	
+	public void setScrollSpeed(int scrollSpeed) {
+		this.scrollSpeed = scrollSpeed;
+	}
+
+	
+	public static int getLinienAbstand() {
 		return linienAbstand;
 	}
 
-	private void setLinienAbstand(int linienAbstand) {
-		this.linienAbstand = linienAbstand;
+	
+	public static void setLinienAbstand(int linienAbstand) {
+		Line.linienAbstand = linienAbstand;
 	}
 
+	
+	public Image getImageL() {
+		return imageL;
+	}
+
+	public void setImageL(Image imageL) {
+		this.imageL = imageL;
+	}
+
+	public Image getImageR() {
+		return imageR;
+	}
+
+	public void setImageR(Image imageR) {
+		this.imageR = imageR;
+	}
+
+	public static int getX() {
+		return x;
+	}
+
+	public static void setX(int x) {
+		Line.x = x;
+	}
+
+	public int getLastZahl() {
+		return lastZahl;
+	}
+
+	public void setLastZahl(int lastZahl) {
+		this.lastZahl = lastZahl;
+	}
+	
+	
 }
